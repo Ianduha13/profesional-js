f5257aefa929149887b9eccc613c1465d51c9bed
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-02-03 14:40:53 -0300

    Challenge done

--- website/assets/MediaPlayer.js
+++ website/assets/MediaPlayer.js
@@ -1,20 +1,38 @@
-function mediaPlayer(config) {
-	this.media = config.el
+function MediaPlayer(config) {
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 }
 
-mediaPlayer.prototype.play = function () {
-	this.media.play()
-}
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this);
+  });
+};
 
-mediaPlayer.prototype.pause = function () {
-	this.media.pause()
-}
+MediaPlayer.prototype.play = function() {
+  this.media.play();
+};
 
-mediaPlayer.prototype.togglePlay = function () {
-	if (this.media.paused) {
-		this.play()
-	} else {
-		this.pause()
-	}
-}
-export default mediaPlayer
+MediaPlayer.prototype.pause = function() {
+  this.media.pause();
+};
+
+MediaPlayer.prototype.togglePlay = function() {
+  if (this.media.paused) {
+    this.play();
+  } else {
+    this.pause();
+  }
+};
+
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false;
+};
+
+export default MediaPlayer;
