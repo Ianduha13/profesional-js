444b7e226174390323c705a17ce416c705759cb0
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-02-04 18:53:29 -0300

    This class done

--- website/assets/index.js
+++ website/assets/index.js
@@ -1,17 +1,20 @@
-import MediaPlayer from './MediaPlayer.js';
-import AutoPlay from './plugins/AutoPlay.js';
+import MediaPlayer from "./MediaPlayer.js"
+import AutoPlay from "./plugins/AutoPlay.js"
 
-const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const video = document.querySelector("video")
+const player = new MediaPlayer({
+	el: video,
+	// plugins: [new AutoPlay()]
+})
 
-const playButton = document.querySelector('#playButton');
-playButton.onclick = () => player.togglePlay();
+const playButton = document.querySelector("#playButton")
+playButton.onclick = () => player.togglePlay()
 
-const muteButton = document.querySelector('#muteButton');
+const muteButton = document.querySelector("#muteButton")
 muteButton.onclick = () => {
-  if (player.media.muted) {
-    player.unmute();
-  } else {
-    player.mute();
-  }
-};
+	if (player.media.muted) {
+		player.unmute()
+	} else {
+		player.mute()
+	}
+}
