b65dc95d4533547947adf2f879560113a3d44ac5
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-02-05 04:06:45 -0300

    Getters and setters class done

--- website/assets/MediaPlayer.js
+++ website/assets/MediaPlayer.js
@@ -1,38 +1,49 @@
 function MediaPlayer(config) {
-  this.media = config.el;
-  this.plugins = config.plugins || [];
+	this.media = config.el
+	this.plugins = config.plugins || []
 
-  this._initPlugins();
+	this._initPlugins()
 }
 
-MediaPlayer.prototype._initPlugins = function() {
-  this.plugins.forEach(plugin => {
-    plugin.run(this);
-  });
-};
+MediaPlayer.prototype._initPlugins = function () {
+	const player = {
+		play: () => this.play(),
+		pause: () => this.pause(),
+		media: this.media,
+		get muted() {
+			return this.media.muted
+		},
+		set muted(value) {
+			this.media.muted = value
+		},
+	}
+	this.plugins.forEach((plugin) => {
+		plugin.run(player)
+	})
+}
 
-MediaPlayer.prototype.play = function() {
-  this.media.play();
-};
+MediaPlayer.prototype.play = function () {
+	this.media.play()
+}
 
-MediaPlayer.prototype.pause = function() {
-  this.media.pause();
-};
+MediaPlayer.prototype.pause = function () {
+	this.media.pause()
+}
 
-MediaPlayer.prototype.togglePlay = function() {
-  if (this.media.paused) {
-    this.play();
-  } else {
-    this.pause();
-  }
-};
+MediaPlayer.prototype.togglePlay = function () {
+	if (this.media.paused) {
+		this.play()
+	} else {
+		this.pause()
+	}
+}
 
-MediaPlayer.prototype.mute = function() {
-  this.media.muted = true;
-};
+MediaPlayer.prototype.mute = function () {
+	this.media.muted = true
+}
 
-MediaPlayer.prototype.unmute = function() {
-  this.media.muted = false;
-};
+MediaPlayer.prototype.unmute = function () {
+	this.media.muted = false
+}
 
-export default MediaPlayer;
+export default MediaPlayer
--- website/assets/plugins/AutoPlay.js
+++ website/assets/plugins/AutoPlay.js
@@ -1,7 +1,9 @@
 function AutoPlay() {}
-AutoPlay.prototype.run = function(player) {
-  player.mute();
-  player.play();
-};
+AutoPlay.prototype.run = function (player) {
+	if (!player.muted) {
+		player.muted = true
+	}
+	player.play()
+}
 
-export default AutoPlay;
+export default AutoPlay
